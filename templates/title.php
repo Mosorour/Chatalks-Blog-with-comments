2fdb2dc7a01cea65790804beed1f38678a1a332e
Mohamed Sorour <mohammedyasser827@gmail.com>
2024-07-14 02:21:54 +0300

    Push the whole blog project

---
+++ templates/title.php
@@ -0,0 +1,6 @@
+<?php require 'top-menu.php' ?>
+
+<a href="index.php">
+    <h1>Chatalks</h1>
+</a>
+<p>Welcome to your space. Login and enjoy the great time sharing your thoughts.<br>HERE!! You can find friends and people to enjoy your time with and exchange experience</p>
